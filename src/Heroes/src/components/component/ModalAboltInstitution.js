7d0212509d8cb69202aef4350970d2c97ffdb4d7
Marcos <marcos.dias.1457746@sga.pucminas.br>
2024-04-21 19:47:54 -0300

    Inicio da criação dos comentarios da instituição

---
+++ src/Heroes/src/components/component/ModalAboltInstitution.js
@@ -0,0 +1,10 @@
+import {View, StyleSheet, Image,Text, TouchableOpacity, ScrollView, Modal} from 'react-native'
+import CardInstitution from './CardsInstitution'
+
+export default function ModalAboltInstitution(){
+    return(
+        <View>
+            
+        </View>
+    )
+}
