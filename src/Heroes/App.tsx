7e92e1a3586e287526b5876a41a1206833640ba6
Wesley Correa Gomes <128870872+WesleyCG22@users.noreply.github.com>
2024-04-21 11:06:04 -0300

    Update App.tsx

--- src/Heroes/App.tsx
+++ src/Heroes/App.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import {
-  StyleSheet,
-  View
-} from 'react-native';
 import Login_SignUp from './src/components/screens/Login_SignUp'
 import { AuthProvider } from './src/components/services/AuthProvider'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import Home from './src/components/screens/Home';
 import NavBar from './src/components/screens/NavBar'
 import { NavigationContainer } from '@react-navigation/native';
 
